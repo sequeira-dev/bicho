7b84e2e7bd86d857436e091cba71a3f00e34bf89
Gustavo Sequeira <gugasequeira-dev@gmail.com>
2025-02-17 16:54:28 -0300

    refatoração da importação

--- uUtil.pas
+++ uUtil.pas
@@ -12,17 +12,18 @@ uses
 
 procedure importarResultados(pDataInicio: TDateTime; pImportarTudoEmDiante: Boolean); // pImportarTudoEmDiante = Se sim, vai trazer tudo apartir da pDataInicio. Se não, apenas a pDataInicio
 var
-  RESTClient: TRESTClient;
-  RESTRequest: TRESTRequest;
-  RESTResponse: TRESTResponse;
-  JSONEnvio: TJSONObject;
-  RetornoJSON: TJSONObject;
-  DrawObj, PreviousDrawObj: TJSONObject;
-  RetornoTexto: string;
-  DrawsArray, PreviousDrawsArray, RaffledPopArray: TJSONArray;
+  vRESTClient: TRESTClient;
+  vRESTRequest: TRESTRequest;
+  vRESTResponse: TRESTResponse;
+  vJSONEnvio: TJSONObject;
+  vRetornoJSON: TJSONObject;
+  vDrawObj, vPreviousDrawObj: TJSONObject;
+  vRetornoTexto: string;
+  vDrawsArray, vPreviousDrawsArray, vRaffledPopArray: TJSONArray;
   I, J, K, vPremio1, vPremio2, vPremio3, vPremio4, vPremio5, vGrupo1, vGrupo2, vGrupo3, vGrupo4, vGrupo5: Integer;
-  FormatSettings: TFormatSettings;
+  vFormatSettings: TFormatSettings;
   vDataInicio: TDateTime;
+  vUltimaDataApagada: Boolean;
 begin
 
   vPremio1 := 0;
@@ -40,115 +41,131 @@ begin
   vPremio5 := 0;
   vGrupo5 := 0;
 
+  vUltimaDataApagada := False;
+
   // Criando os objetos REST
-  RESTClient := TRESTClient.Create('https://www.akyloterias.com/web/lotericos/svc/resultados/loterias');
-  RESTRequest := TRESTRequest.Create(nil);
-  RESTResponse := TRESTResponse.Create(nil);
+  vRESTClient := TRESTClient.Create('https://www.akyloterias.com/web/lotericos/svc/resultados/loterias');
+  vRESTRequest := TRESTRequest.Create(nil);
+  vRESTResponse := TRESTResponse.Create(nil);
 
   try
     try
-
       // Configurando o Request
-      RESTRequest.Client := RESTClient;
-      RESTRequest.Response := RESTResponse;
-      RESTRequest.Method := rmPOST;
+      vRESTRequest.Client := vRESTClient;
+      vRESTRequest.Response := vRESTResponse;
+      vRESTRequest.Method := rmPOST;
 
-      FormatSettings := TFormatSettings.Create;
-      FormatSettings.DateSeparator := '-';
-      FormatSettings.ShortDateFormat := 'yyyy-mm-dd';
+      vFormatSettings := TFormatSettings.Create;
+      vFormatSettings.DateSeparator := '-';
+      vFormatSettings.ShortDateFormat := 'yyyy-mm-dd';
 
       vDataInicio := pDataInicio;
 
       while vDataInicio <= Now do
       begin
 
+        // verifica se existe todos os premios, caso seja verdadeiro vai pra data seguinte
+        if not(uUtilBanco.dataIncluida(vDataInicio)) then
+        begin
+
+          // Necessario, porque ainda não tem como passar o parametro na requisao com o horario, sendo assim, apaga-se todos os resultados do dia
+          if (vDataInicio = uUtilBanco.pegaUltimaDataResultado) and (vUltimaDataApagada = False) then
+          begin
+            uUtilBanco.apagarUltimaDataResultado(vDataInicio);
+            vUltimaDataApagada := True;
+          end;
+
           // Criando JSON de envio
-        JSONEnvio := TJSONObject.Create;
+          vJSONEnvio := TJSONObject.Create;
 
-        try
-          JSONEnvio.AddPair('strData', DateToStr(vDataInicio, FormatSettings));
+          try
+            vJSONEnvio.AddPair('strData', DateToStr(vDataInicio, vFormatSettings));
 
-          RESTRequest.Params.Clear;  // Limpa parâmetros
-          RESTRequest.ClearBody;
+          // Limpa parâmetros
+            vRESTRequest.Params.Clear;
+
+          // Limpa o body;
+            vRESTRequest.ClearBody;
 
             // Adicionando JSON ao Body
-          RESTRequest.AddBody(JSONEnvio.ToJSON, TRESTContentType.ctAPPLICATION_JSON);
+            vRESTRequest.AddBody(vJSONEnvio.ToJSON, TRESTContentType.ctAPPLICATION_JSON);
 
 
             // Executando a requisição
-          RESTRequest.Execute;
+            vRESTRequest.Execute;
 
             // Pegando o retorno da API
-          RetornoTexto := RESTResponse.Content;  // Retorno como string
+            vRetornoTexto := vRESTResponse.Content;  // Retorno como string
 
             // Convertendo para um objeto JSON
-          RetornoJSON := TJSONObject.ParseJSONValue(RetornoTexto) as TJSONObject;
-          if Assigned(RetornoJSON) then
-          begin
+            vRetornoJSON := TJSONObject.ParseJSONValue(vRetornoTexto) as TJSONObject;
+            if Assigned(vRetornoJSON) then
+            begin
 
               // Pegar a lista de "draws"
-            DrawsArray := RetornoJSON.GetValue<TJSONArray>('draws');
-            if not Assigned(DrawsArray) then
-              Exit;
+              vDrawsArray := vRetornoJSON.GetValue<TJSONArray>('draws');
+              if not Assigned(vDrawsArray) then
+                Exit;
 
-            for I := 0 to DrawsArray.Count - 1 do
-            begin
-              DrawObj := DrawsArray.Items[I] as TJSONObject;
+              for I := 0 to vDrawsArray.Count - 1 do
+              begin
+                vDrawObj := vDrawsArray.Items[I] as TJSONObject;
 
                 // Pegar a lista de "previousDraws"
-              PreviousDrawsArray := DrawObj.GetValue<TJSONArray>('previousDraws');
-              if not Assigned(PreviousDrawsArray) then
-                Continue;
+                vPreviousDrawsArray := vDrawObj.GetValue<TJSONArray>('previousDraws');
+                if not Assigned(vPreviousDrawsArray) then
+                  Continue;
 
                 // Percorrer "previousDraws"
-              for J := 0 to PreviousDrawsArray.Count - 1 do
-              begin
-                PreviousDrawObj := PreviousDrawsArray.Items[J] as TJSONObject;
-                  // Obter array "raffledPop"
-                RaffledPopArray := PreviousDrawObj.GetValue<TJSONArray>('raffledPop');
-
-                if Assigned(RaffledPopArray) then
+                for J := 0 to vPreviousDrawsArray.Count - 1 do
                 begin
+                  vPreviousDrawObj := vPreviousDrawsArray.Items[J] as TJSONObject;
+                  // Obter array "raffledPop"
+                  vRaffledPopArray := vPreviousDrawObj.GetValue<TJSONArray>('raffledPop');
 
-                  for K := 0 to RaffledPopArray.Count - 1 do
+                  if Assigned(vRaffledPopArray) then
                   begin
-                    case K of
-                      0:
-                        begin
-                          vPremio1 := StrToInt(copy(RaffledPopArray.Items[K].Value, 1, 4));
-                          vGrupo1 := StrToInt(copy(RaffledPopArray.Items[K].Value, 6, 2));
-                        end;
-                      1:
-                        begin
-                          vPremio2 := StrToInt(copy(RaffledPopArray.Items[K].Value, 1, 4));
-                          vGrupo2 := StrToInt(copy(RaffledPopArray.Items[K].Value, 6, 2));
-                        end;
-                      2:
-                        begin
-                          vPremio3 := StrToInt(copy(RaffledPopArray.Items[K].Value, 1, 4));
-                          vGrupo3 := StrToInt(copy(RaffledPopArray.Items[K].Value, 6, 2));
-                        end;
-                      3:
-                        begin
-                          vPremio4 := StrToInt(copy(RaffledPopArray.Items[K].Value, 1, 4));
-                          vGrupo4 := StrToInt(copy(RaffledPopArray.Items[K].Value, 6, 2));
-                        end;
-                      4:
-                        begin
-                          vPremio5 := StrToInt(copy(RaffledPopArray.Items[K].Value, 1, 4));
-                          vGrupo5 := StrToInt(copy(RaffledPopArray.Items[K].Value, 6, 2));
-                        end;
+
+                    for K := 0 to vRaffledPopArray.Count - 1 do
+                    begin
+                      case K of
+                        0:
+                          begin
+                            vPremio1 := StrToInt(copy(vRaffledPopArray.Items[K].Value, 1, 4));
+                            vGrupo1 := StrToInt(copy(vRaffledPopArray.Items[K].Value, 6, 2));
+                          end;
+                        1:
+                          begin
+                            vPremio2 := StrToInt(copy(vRaffledPopArray.Items[K].Value, 1, 4));
+                            vGrupo2 := StrToInt(copy(vRaffledPopArray.Items[K].Value, 6, 2));
+                          end;
+                        2:
+                          begin
+                            vPremio3 := StrToInt(copy(vRaffledPopArray.Items[K].Value, 1, 4));
+                            vGrupo3 := StrToInt(copy(vRaffledPopArray.Items[K].Value, 6, 2));
+                          end;
+                        3:
+                          begin
+                            vPremio4 := StrToInt(copy(vRaffledPopArray.Items[K].Value, 1, 4));
+                            vGrupo4 := StrToInt(copy(vRaffledPopArray.Items[K].Value, 6, 2));
+                          end;
+                        4:
+                          begin
+                            vPremio5 := StrToInt(copy(vRaffledPopArray.Items[K].Value, 1, 4));
+                            vGrupo5 := StrToInt(copy(vRaffledPopArray.Items[K].Value, 6, 2));
+                          end;
+                      end;
                     end;
                   end;
+
+                  uUtilBanco.incluirResultado(StrToDateTime(vPreviousDrawObj.GetValue<string>('timestamp'), vFormatSettings), vPreviousDrawObj.GetValue<string>('raffledDescription'), vPremio1, vPremio2, vPremio3, vPremio4, vPremio5, vGrupo1, vGrupo2, vGrupo3, vGrupo4, vGrupo5);
+
                 end;
-
-                uUtilBanco.incluirResultado(StrToDateTime(PreviousDrawObj.GetValue<string>('timestamp'), FormatSettings), PreviousDrawObj.GetValue<string>('raffledDescription'), vPremio1, vPremio2, vPremio3, vPremio4, vPremio5, vGrupo1, vGrupo2, vGrupo3, vGrupo4, vGrupo5);
-
               end;
             end;
+          finally
+            vJSONEnvio.Free;
           end;
-        finally
-          JSONEnvio.Free;
         end;
 
         vDataInicio := vDataInicio + 1;
@@ -162,10 +179,10 @@ begin
     end;
   finally
     // Liberando memória
-    RESTRequest.Free;
-    RESTResponse.Free;
-    RESTClient.Free;
-    RetornoJSON.Free;
+    vRESTRequest.Free;
+    vRESTResponse.Free;
+    vRESTClient.Free;
+    vRetornoJSON.Free;
   end;
 
 end;
--- uUtilBanco.pas
+++ uUtilBanco.pas
@@ -2,16 +2,47 @@ unit uUtilBanco;
 
 interface
 
-procedure incluirResultado(pData: TDateTime; pDescricao: String; pPremio1: Integer; pPremio2: Integer; pPremio3: Integer; pPremio4: Integer; pPremio5: Integer;
-                      pGrupo1: Integer; pGrupo2: Integer; pGrupo3: Integer; pGrupo4: Integer; pGrupo5: Integer);
+
+  //Procedures
+  procedure apagarUltimaDataResultado(pData: TDateTime);
+  procedure incluirResultado(pData: TDateTime; pDescricao: String; pPremio1: Integer; pPremio2: Integer; pPremio3: Integer; pPremio4: Integer; pPremio5: Integer; pGrupo1: Integer; pGrupo2: Integer; pGrupo3: Integer; pGrupo4: Integer; pGrupo5: Integer);
+
+  //Functions
+  function pegaUltimaDataResultado: TDateTime;
+  function dataIncluida(pData: TDateTime): Boolean;
+  function ehDiaFederal(pData: TDateTime): Boolean;
 
 implementation
 
 uses
   FireDAC.Comp.Client, uDmPrincipal, System.SysUtils, FireDAC.Stan.Param;
 
-procedure incluirResultado(pData: TDateTime; pDescricao: String; pPremio1: Integer; pPremio2: Integer; pPremio3: Integer; pPremio4: Integer; pPremio5: Integer;
-                      pGrupo1: Integer; pGrupo2: Integer; pGrupo3: Integer; pGrupo4: Integer; pGrupo5: Integer);
+procedure apagarUltimaDataResultado(pData: TDateTime);
+var
+  FormatSettings: TFormatSettings;
+begin
+
+  FormatSettings := TFormatSettings.Create;
+  FormatSettings.DateSeparator := '-';
+  FormatSettings.ShortDateFormat := 'yyyy-mm-dd';
+
+  with dmPrincipal do
+  begin
+    FDQuery.SQL.Text :=
+    '	delete ' +
+    '	  from resultado ' +
+    '	 where data::date = '+QuotedStr(DateToStr(pData, FormatSettings));
+
+    try
+      FDQuery.ExecSQL;
+    except
+      on e:Exception do
+      raise Exception.Create('Erro ao tentar excluir resultado. '+e.Message);
+    end;
+  end;
+end;
+
+procedure incluirResultado(pData: TDateTime; pDescricao: String; pPremio1: Integer; pPremio2: Integer; pPremio3: Integer; pPremio4: Integer; pPremio5: Integer; pGrupo1: Integer; pGrupo2: Integer; pGrupo3: Integer; pGrupo4: Integer; pGrupo5: Integer);
 begin
   with dmPrincipal do
   begin
@@ -38,9 +69,86 @@ begin
       FDQuery.ExecSQL;
     except
       on e:Exception do
-      raise Exception.Create('Erro ao tentar incluir jogo. '+e.Message);
+      raise Exception.Create('Erro ao tentar incluir resultado. '+e.Message);
+    end;
+  end;
+end;
+
+function pegaUltimaDataResultado: TDateTime;
+begin
+  Result := Now;
+  with dmPrincipal do
+  begin
+    FDQuery.SQL.Text := ' select max(data) as data from resultado ';
+    try
+      FDQuery.Open;
+      Result := FDQuery.FieldByName('data').AsDateTime;
+    except
+      on e:Exception do
+      raise Exception.Create('Erro ao tentar consultar resultado. '+e.Message);
+    end;
+  end;
+end;
+
+function dataIncluida(pData: TDateTime): Boolean;
+var
+  FormatSettings: TFormatSettings;
+begin
+
+  FormatSettings := TFormatSettings.Create;
+  FormatSettings.DateSeparator := '-';
+  FormatSettings.ShortDateFormat := 'yyyy-mm-dd';
+  Result := False;
+
+  with dmPrincipal do
+  begin
+    FDQuery.SQL.Text := '  	select count(data) '+
+     	  	              '     from resultado   '+
+			                  ' where data::date = '+QuotedStr(DateToStr(pData, FormatSettings));
+    try
+      FDQuery.Open;
+
+
+      if ehDiaFederal(pData) then
+        Result := FDQuery.Fields[0].AsInteger = 8 // quantidades de horarios + FEDERAL
+      else
+        Result := FDQuery.Fields[0].AsInteger = 7; // quantidades de horarios
+
+    except
+      on e:Exception do
+      raise Exception.Create('Erro ao tentar consultar resultado. '+e.Message);
+    end;
+  end;
+end;
+
+
+function ehDiaFederal(pData: TDateTime): Boolean;
+var
+  FormatSettings: TFormatSettings;
+begin
+
+  FormatSettings := TFormatSettings.Create;
+  FormatSettings.DateSeparator := '-';
+  FormatSettings.ShortDateFormat := 'yyyy-mm-dd';
+
+  Result := False;
+
+  with dmPrincipal do
+  begin
+    FDQuery.SQL.Text := ' select distinct 1 '+
+                    	  '	  from resultado ' +
+			                  '  where EXTRACT(DOW FROM '+QuotedStr(DateToStr(pData, FormatSettings))+') IN (3, 6) ';//3 - quarta e 6 - sabado
+    try
+      FDQuery.Open;
+      Result := not(FDQuery.IsEmpty);
+    except
+      on e:Exception do
+      raise Exception.Create('Erro ao tentar consultar resultado. '+e.Message);
     end;
   end;
 end;
 
 end.
+
+
+
